53dc73e59193d9e9ecda3931889063f46969ae88
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-09-19 14:30:29 +0500

    Delete out directory

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Wed Sep 18 04:17:15 UTC 2024"
-#define LINUX_COMPILE_BY "gitpod"
-#define LINUX_COMPILE_HOST "lineageos-androidkernel-pfecadwpm0r"
-#define LINUX_COMPILER "Android (11039501, +pgo, +bolt, +lto, +mlgo, based on r498229b) clang version 17.0.4 (https://android.googlesource.com/toolchain/llvm-project e34ed7d63863b45858e74126edaa738e75887800), GNU ld (binutils-2.27-bd24d23f) 2.27.0.20170315"
